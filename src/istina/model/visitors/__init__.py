71f64e6ff983f4effc0da6d243f1c1a6e7b396ba
Fahad Khan <fk12153180@gmail.com>
2026-03-03 20:17:12 -0500

    feat(issue 7.1): defined article visitor interface

--- src/istina/model/visitors/__init__.py
+++ src/istina/model/visitors/__init__.py
@@ -13,3 +13,8 @@ Visitors:
 - ArticleVisitor: base visitor interface for Article traversal
 - ScoringVisitor: produces BiasScore (or triggers provider calls)
 """
+
+from istina.model.visitors.article_visitor import ArticleVisitor
+from istina.model.visitors.scoring_visitor import ScoringVisitor
+
+__all__ = ["ArticleVisitor", "ScoringVisitor"]
